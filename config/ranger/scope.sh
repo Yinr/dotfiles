5f6eb395fa383202b15e9c63256c7c798d06cd2a
Yinr <yinr@yinr.cc>
2019-11-10 22:20:29 +0800

    update: ranger: not preview svg as image

--- config/ranger/scope.sh
+++ config/ranger/scope.sh
@@ -89,10 +89,10 @@ handle_extension() {
 handle_image() {
     local mimetype="${1}"
     case "${mimetype}" in
-        # SVG
-        # image/svg+xml)
-        #     convert "${FILE_PATH}" "${IMAGE_CACHE_PATH}" && exit 6
-        #     exit 1;;
+        SVG
+        image/svg+xml)
+            convert "${FILE_PATH}" "${IMAGE_CACHE_PATH}" && exit 6
+            exit 1;;
 
         # Image
         image/*)
