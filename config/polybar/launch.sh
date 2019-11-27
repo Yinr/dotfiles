7dd9aeb3400dabea915d16f782204017ee35ee2c
Yinr <yinr@yinr.cc>
2019-11-27 17:16:34 +0800

    update: polybar: multiple update

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,10 +1,18 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+export MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
+
+dev_net="$(printf '%s\n' /sys/class/net/*/device | awk -F'/' '{ print $5 }')"
+dev_wl="$(printf '%s\n' /sys/class/net/*/wireless | awk -F'/' '{ print $5 }')"
+
+export DEVICE_ETHERNET="$(sort <(echo $dev_net) <(echo $dev_wl) <(echo $dev_wl)|uniq -u|head -1)"
+export DEVICE_WIRELESS="$(echo $dev_wl|head -1)"
 
 polybar main &
 
