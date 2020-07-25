5fcef5b4bdc322740324b7c4915b997008753300
Yinr <yinr@yinr.cc>
2020-07-25 16:03:23 +0800

    fixup: polybar: fixup monitor & temp & eth device

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,13 +6,17 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-export MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
+MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
+export MONITOR=${MONITOR:-$(xrandr | sed -En '/connected primary/s/^([^ ]+).*/\1/p')}
+
+export TEMP_HWMON_PATH=$(ls /sys/devices/platform/coretemp.0/hwmon/hwmon*/temp1_input|head -1)
 
 dev_net="$(printf '%s\n' /sys/class/net/*/device | awk -F'/' '{ print $5 }')"
-dev_wl="$(printf '%s\n' /sys/class/net/*/wireless | awk -F'/' '{ print $5 }')"
+dev_wl="$(printf '%s\n' /sys/class/net/*/wireless | awk -F'/' '{ print $5 }' | head -1)"
+dev_net=$(echo "${dev_net}\n${dev_wl}\n${dev_wl}" | sort | uniq -u | head -1)
 
-export DEVICE_ETHERNET="$(sort <(echo $dev_net) <(echo $dev_wl) <(echo $dev_wl)|uniq -u|head -1)"
-export DEVICE_WIRELESS="$(echo $dev_wl|head -1)"
+export DEVICE_ETHERNET="${dev_net}"
+export DEVICE_WIRELESS="${dev_wl}"
 
 polybar main &
 
